4184c90aec2579104f0237c82a2c880978786571
Trevor Stanhope <trevor@W510>
2013-12-23 19:05:48 -0500

    report

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install python -y
+sudo apt-get install python-zmq -y
+sudo apt-get install python-serial -y
+sudo apt-get install python-cherrypy3 -y
+sudo apt-get install python-pyaudio -y
+sudo apt-get install python-opencv -y
+sudo apt-get install python-couchdb -y
+sudo apt-get install pip -y
+sudo pip install firebase
+sudo apt-get install couchdb -y
