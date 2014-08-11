5eee808a0d6a8f6e3eeb9a956374cc2b861d6599
Trevor Stanhope <tpstanhope@gmail.com>
2014-08-11 15:44:39 -0400

    permission fix

--- install.sh
+++ install.sh
@@ -37,5 +37,5 @@ sudo cp configs/interfaces /etc/network
 # Start on boot
 sudo cp -R ../hive-node /usr/share/
 sudo ln -s /usr/share/hive-node/bin/hive-node /usr/bin
-chmod +x /usr/share/hive-node/bin/hive-node
+sudo chmod +x /usr/share/hive-node/bin/hive-node
 sudo cp configs/rc.local /etc/
