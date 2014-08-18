71711499db2475208845283baf3e6d09fe1618ce
Evan Henry <evan@T530>
2014-08-18 14:36:11 -0400

    prompt for sections

--- install.sh
+++ install.sh
@@ -15,7 +15,11 @@ sudo apt-get install python-opencv -y
 sudo apt-get install libasound2-dev alsa-utils -y
 
 # Alamode
-echo "Setting up Arduino ..."
+#echo "Setting up Arduino ..."
+echo "Would you like to install Alamode and libraries [y/n]?"
+read ans
+if [ $ans = y -o $ans = Y -o $ans = yes -o $ans = Yes -o $ans = YES ]
+then
 sudo cp configs/avrdude /usr/bin/avrdude
 sudo cp configs/avrdude /usr/share/arduino/hardware/tools
 sudo cp configs/avrdude.conf  /usr/share/arduino/hardware/tools
@@ -28,17 +32,48 @@ sudo chgrp root /usr/bin/avrdude /usr/share/arduino/hardware/tools/avrdude
 sudo chmod a+s /usr/bin/avrdude /usr/share/arduino/hardware/tools/avrdude
 sudo cp -r libs/* /usr/share/arduino/libraries
 sudo cp /usr/share/arduino/libraries/Wire/utility/* /usr/share/arduino/libraries/Wire/
+fi
+if [ $ans = n -o $ans = N -o $ans = no -o $ans = No -o $ans = NO ]
+then
+echo "Skipping Arduino install"
+fi
 
 # Connect to local wireless network
-echo "Setting up network configuration ..."
+echo "Would you like to configure the network [y/n]?"
+read ans
+if [ $ans = y -o $ans = Y -o $ans = yes -o $ans = Yes -o $ans = YES ]
+then
 sudo cp configs/interfaces /etc/network
+fi
+if [ $ans = n -o $ans = N -o $ans = no -o $ans = No -o $ans = NO ]
+then
+echo "Skipping network configuration..."
+fi
 
 # Install to path
+echo "Would you like to install the program to path [y/n]?"
+read ans
+if [ $ans = y -o $ans = Y -o $ans = yes -o $ans = Yes -o $ans = YES ]
+then
 echo "Installing to /usr/share ..."
 sudo cp -R ../hive-node /usr/share/
 sudo ln -sf /usr/share/hive-node/bin/hive-node /usr/bin
-sudo chmod +x /usr/share/hive-node/bin/hive-node
+sudo chmod +x /usr/share/hive-node/bin/hive-nodefi
+fi
+if [ $ans = n -o $ans = N -o $ans = no -o $ans = No -o $ans = NO ]
+then
+echo "Skipping install to path..."
+fi
 
 # Start on boot
+echo "Would you like the node to start on boot [y/n]?"
+read ans
+if [ $ans = y -o $ans = Y -o $ans = yes -o $ans = Yes -o $ans = YES ]
+then
 echo "Setting up start on boot ..."
-sudo cp configs/rc.local /etc/
+sudo cp configs/rc.local /etc/fi
+fi
+if [ $ans = n -o $ans = N -o $ans = no -o $ans = No -o $ans = NO ]
+then
+echo "Skipping start on boot..."
+fi
