ae9c19aeb45a6ce24830688318e41f9f4e19bb9e
evanhenry <evan.henry@mail.mcgill.ca>
2015-06-14 14:49:23 -0700

    updated audio

--- hive-node.py
+++ hive-node.py
@@ -37,15 +37,11 @@ import logging
 import socket
 import cv2
 #audio libraries
-import matplotlib
-matplotlib.use('TkAgg') # THIS MAKES IT FAST!
-#import numpy
-import scipy
-import struct
+from matplotlib.mlab import find
+from matplotlib import pyplot as plt
 import pyaudio
-import threading
-import pylab
-import struct
+import numpy as np
+import math
 
 try:
     import Adafruit_DHT
@@ -85,12 +81,14 @@ class HiveNode:
             self.ARDUINO_BAUD = 9600
             self.ARDUINO_TIMEOUT = 3
             self.MICROPHONE_CHANNELS = 1
-            self.MICROPHONE_RATE = 48000
+            self.MICROPHONE_RATE = 44100
             self.MICROPHONE_CHUNK = 4096
             self.MICROPHONE_FORMAT = 8
+			self.MICROPHONE_RECORD_SECONDS = 4
+			self.MICROPHONE_LOWPASS = 1000 # hz
             self.CAMERA_INDEX = 0
             self.CHERRYPY_PORT = 8081
-            self.CHERRYPY_ADDR ="0.0.0.0"
+            self.CHERRYPY_ADDR = "0.0.0.0"
             self.PING_INTERVAL = 1
             self.LOG_FILE = "log.txt"
             self.PARAMS = ["int_t","ext_t","int_h","ext_h","volts","amps","hz","db","pa"]
@@ -193,72 +191,74 @@ class HiveNode:
             self.log_msg('CAM', 'Error: %s' % str(error))
 	
     ## Initialize audio  	
-    def init_mic(self):
-		""" part of revised code for audio processing """
-        self.BUFFERSIZE = 2**12 # 1024 is a good buffer size
-        self.secToRecord = 0.1
-        self.threadsDieNow = False
-        self.newAudio = False
-		self.buffersToRecord = int(self.RATE * self.secToRecord / self.BUFFERSIZE)
-        if self.buffersToRecord == 0:
-			self.buffersToRecord = 1
-        self.samplesToRecord = int(self.BUFFERSIZE * self.buffersToRecord)
-        self.chunksToRecord = int(self.samplesToRecord / self.BUFFERSIZE)
-        self.secPerPoint = 1.0 / self.MICROPHONE_RATE
-		
-		# Start audio stream
-        self.p = pyaudio.PyAudio()
-        self.microphone = self.p.open(
-			format=pyaudio.paInt16,
-			channels=1,
-            rate=self.MICROPHONE_RATE,
-			input=True,
-			frames_per_buffer=self.BUFFERSIZE
-			)
-        self.xsBuffer=numpy.arange(self.BUFFERSIZE)*self.secPerPoint
-        self.xs=numpy.arange(self.chunksToRecord*self.BUFFERSIZE)*self.secPerPoint
-        self.audio=numpy.empty((self.chunksToRecord*self.BUFFERSIZE),dtype=numpy.int16)      
-		
-		# Start Recording in background
-		self.t = threading.Thread(target=self.record)
-        self.t.start()
+	def __init__(self, config=None):
+		# Configuration
+		if not config:
+			self.MICROPHONE_CHANNELS = 1
+			self.MICROPHONE_RATE = 44100
+			self.MICROPHONE_CHUNK = 4096
+			self.MICROPHONE_FORMAT = 8
+			self.MICROPHONE_RECORD_SECONDS = 2
+			self.MICROPHONE_LOWPASS = 1000 # hz
+			
+		# Initializers
+		try:
+			self.init_mic()
+		except Exception as e:
+			print str(e)
 
+	## Initialize audio
+	def init_mic(self):
+		""" part of revised code for audio processing """
+		# Start audio stream
+		try:
+			self.p = pyaudio.PyAudio()
+			self.microphone = self.p.open(
+				format=pyaudio.paInt16,
+				channels=self.MICROPHONE_CHANNELS,
+				rate=self.MICROPHONE_RATE,
+				input=True,
+				frames_per_buffer=self.MICROPHONE_CHUNK)
+		except Exception as e:
+			raise e
+			
 	## Close Microphone
-    def close_mic(self):
-        """cleanly back out and release sound card."""
-		self.threadsDieNow=True  
-        self.p.close(self.microphone)
-	
-	## Record
-    def record(self):
-        """ Threaded function to record secToRecord seconds of audio."""
-        while True:
-            if self.threadsDieNow: break
-            for i in range(self.chunksToRecord):
-				audioString = self.microphone.read(self.BUFFERSIZE)
-				audioNumeric = numpy.fromstring(audioString,dtype=numpy.int16)
-                self.audio[i*self.BUFFERSIZE:(i+1)*self.BUFFERSIZE] = audioNumeric
-            self.newAudio=True 
-            if forever==False: break
+	def close_mic(self):
+		"""cleanly back out and release sound card."""
+		self.p.close(self.microphone)
 	
 	# Capture Audio
-    def capture_audio(self, trimBy=10):
+	def capture_audio(self, trimBy=10):
 		db = None
 		hz = None
 		try:
-			data=self.audio.flatten() # pull data from record() thread
-			left,right=numpy.split(numpy.abs(numpy.fft.fft(data)),2)
-			db=numpy.add(left,right[::-1])
-			db=numpy.multiply(20,numpy.log10(ys)) # db
-			hz=numpy.arange(self.BUFFERSIZE/2,dtype=float) # hz
-			if trimBy:
-				i=int((self.BUFFERSIZE/2)/trimBy)
-				db=db[:i]
-				hz=hz[:i]*self.RATE/self.BUFFERSIZE
+			# Capture Audio and convert to numeric
+			audio = [] 
+			for i in range(0, self.MICROPHONE_RATE / self.MICROPHONE_CHUNK * self.MICROPHONE_RECORD_SECONDS):
+				audioString = self.microphone.read(self.MICROPHONE_CHUNK)
+				audioNumeric = np.fromstring(audioString,dtype=np.int16)
+				audio.append(audioNumeric)
+			
+			# Calculate Pitch
+			pitch = []
+			for signal in audio:
+				crossing = [math.copysign(1.0, s) for s in signal]
+				index = find(np.diff(crossing));
+				f0 = round(len(index) * self.MICROPHONE_RATE /(2.0 * np.prod(len(signal))))
+				pitch.append(f0)
+			pitch = np.array(pitch)
+			pitch_lowpass = pitch[pitch < self.MICROPHONE_LOWPASS]
+			hz = np.median(pitch_lowpass)
+			
+			# Calculate Decibels
+			# audio.flatten() # pull data from record() thread
+			left,right=np.split(np.abs(np.fft.fft(audio)),2)
+			db = np.add(left,right[::-1])
+			db = np.multiply(20,np.log10(db)) # db
+			db = np.mean(np.multiply(20,np.log10(db))) # convert to dB
 		except Exception as error:
 			self.log_msg('MIC', 'Error: %s' % str(error))
-        return { "db" : db, "hz" : hz}
-		
+		return { "db" : db, "hz" : hz}
     ## Capture Video
     def capture_video(self):
         self.log_msg('CAM', 'Capturing video ...')
