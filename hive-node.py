73a57fcfa995bab19be828955e947250fb47f27f
evanhenry <evan.henry@mail.mcgill.ca>
2015-06-14 16:15:22 -0700

    indenting and adding mic log msg

--- hive-node.py
+++ hive-node.py
@@ -190,58 +190,59 @@ class HiveNode:
         except Exception as error:
             self.log_msg('CAM', 'Error: %s' % str(error))
 
-	## Initialize audio
-	def init_mic(self):
-		""" part of revised code for audio processing """
-		# Start audio stream
-		try:
-			self.p = pyaudio.PyAudio()
-			self.microphone = self.p.open(
-				format=pyaudio.paInt16,
-				channels=self.MICROPHONE_CHANNELS,
-				rate=self.MICROPHONE_RATE,
-				input=True,
-				frames_per_buffer=self.MICROPHONE_CHUNK)
-		except Exception as e:
-			raise e
-			
-	## Close Microphone
-	def close_mic(self):
-		"""cleanly back out and release sound card."""
-		self.p.close(self.microphone)
-	
-	# Capture Audio
-	def capture_audio(self, trimBy=10):
-		db = None
-		hz = None
-		try:
-			# Capture Audio and convert to numeric
-			audio = [] 
-			for i in range(0, self.MICROPHONE_RATE / self.MICROPHONE_CHUNK * self.MICROPHONE_RECORD_SECONDS):
-				audioString = self.microphone.read(self.MICROPHONE_CHUNK)
-				audioNumeric = np.fromstring(audioString,dtype=np.int16)
-				audio.append(audioNumeric)
-			
-			# Calculate Pitch
-			pitch = []
-			for signal in audio:
-				crossing = [math.copysign(1.0, s) for s in signal]
-				index = find(np.diff(crossing));
-				f0 = round(len(index) * self.MICROPHONE_RATE /(2.0 * np.prod(len(signal))))
-				pitch.append(f0)
-			pitch = np.array(pitch)
-			pitch_lowpass = pitch[pitch < self.MICROPHONE_LOWPASS]
-			hz = np.median(pitch_lowpass)
-			
-			# Calculate Decibels
-			# audio.flatten() # pull data from record() thread
-			left,right=np.split(np.abs(np.fft.fft(audio)),2)
-			db = np.add(left,right[::-1])
-			db = np.multiply(20,np.log10(db)) # db
-			db = np.mean(np.multiply(20,np.log10(db))) # convert to dB
-		except Exception as error:
-			self.log_msg('MIC', 'Error: %s' % str(error))
-		return { "db" : db, "hz" : hz}
+    ## Initialize audio
+    def init_mic(self):
+        self.log_msg('MIC', 'Initializing mic ...')
+        """ part of revised code for audio processing """
+        # Start audio stream
+        try:
+            self.p = pyaudio.PyAudio()
+            self.microphone = self.p.open(
+                format=pyaudio.paInt16,
+                channels=self.MICROPHONE_CHANNELS,
+                rate=self.MICROPHONE_RATE,
+                input=True,
+                frames_per_buffer=self.MICROPHONE_CHUNK)
+        except Exception as e:
+            raise e
+
+    ## Close Microphone
+    def close_mic(self):
+        """cleanly back out and release sound card."""
+        self.p.close(self.microphone)
+
+    # Capture Audio
+    def capture_audio(self, trimBy=10):
+        db = None
+        hz = None
+        try:
+            # Capture Audio and convert to numeric
+            audio = [] 
+            for i in range(0, self.MICROPHONE_RATE / self.MICROPHONE_CHUNK * self.MICROPHONE_RECORD_SECONDS):
+                audioString = self.microphone.read(self.MICROPHONE_CHUNK)
+                audioNumeric = np.fromstring(audioString,dtype=np.int16)
+                audio.append(audioNumeric)
+
+            # Calculate Pitch
+            pitch = []
+            for signal in audio:
+                crossing = [math.copysign(1.0, s) for s in signal]
+                index = find(np.diff(crossing));
+                f0 = round(len(index) * self.MICROPHONE_RATE /(2.0 * np.prod(len(signal)))
+                pitch.append(f0)
+            pitch = np.array(pitch)
+            pitch_lowpass = pitch[pitch < self.MICROPHONE_LOWPASS]
+            hz = np.median(pitch_lowpass)
+
+            # Calculate Decibels
+            # audio.flatten() # pull data from record() thread
+            left,right=np.split(np.abs(np.fft.fft(audio)),2)
+            db = np.add(left,right[::-1])
+            db = np.multiply(20,np.log10(db)) # db
+            db = np.mean(np.multiply(20,np.log10(db))) # convert to dB
+        except Exception as error:
+            self.log_msg('MIC', 'Error: %s' % str(error))
+        return { "db" : db, "hz" : hz}
     ## Capture Video
     def capture_video(self):
         self.log_msg('CAM', 'Capturing video ...')
