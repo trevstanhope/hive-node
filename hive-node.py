audio errno input overflow fix attempt 11: http://stackoverflow.com/questions/29064221/input-overflow-with-pyaudio) rate=44100, chunk=1024, added pass over error and fixed indents 3 fixed error statment

hive-node.py
@@ -218,14 +218,14 @@ class HiveNode:
         try:
             # Capture Audio and convert to numeric
             audio = [] 
-            for i in range(0, self.MICROPHONE_RATE / self.MICROPHONE_CHUNK * self.MICROPHONE_RECORD_SECONDS):
-                print i 
+            for i in range(0, self.MICROPHONE_RATE / self.MICROPHONE_CHUNK * self.MICROPHONE_RECORD_SECONDS): 
                 try:
                     audioString = self.microphone.read(self.MICROPHONE_CHUNK)
                     audioNumeric = np.fromstring(audioString,dtype=np.int16)
                     audio.append(audioNumeric)
                 except IOError:
                     pass
+                audio.append(audioNumeric)
 
             # Calculate Pitch
             pitch = []