8aa1bdc62f705007dff5b6914c631f966020e8ed
Trevor Stanhope <trevor@firebase>
2015-06-24 19:26:01 -0400

    bug

--- hive-node.py
+++ hive-node.py
@@ -85,7 +85,7 @@ class HiveNode:
             self.ARDUINO_TIMEOUT = 3
             self.MICROPHONE_ENABLED = True
             self.MICROPHONE_CHANNELS = 1
-            self.MICROPHONE_RATE = 44100
+            self.MICROPHONE_RATE = 48000
             self.MICROPHONE_CHUNK = 1024
             self.MICROPHONE_FORMAT = 8
             self.MICROPHONE_RECORD_SECONDS = 3
